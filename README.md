52b885623dd93b0191d1bb710d425ee9799c2863
hhhzhao <huazhao@iu.edu>
2019-11-27 15:27:22 -0500

    done

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Project-Template-Godot
 A generic template for a Godot project
+welcome to my game! My name is Hua Zhao. In the game, you are allow to control the ball move around the 'world'. Your job is to push the crate to the ground in order to get scores. 
+You will have 3 mins to finish the job. If you haven't push all the crates to the ground, you will lose. Enjoy the game!
--- World.gd
+++ World.gd
@@ -2,8 +2,9 @@ extends Spatial
 
 onready var Score = $HUD/Score
 onready var Lives = $HUD/Lives
-
+onready var Remaining = $HUD/Remaining
 var score = 0
+var time_remaining = 180
 
 func _unhandled_input(event):
 	if Input.is_action_pressed("quit"):
@@ -12,3 +13,15 @@ func _unhandled_input(event):
 func add_score(s):
 	score += s
 	Score.text = str(score)
+	if score == 110:
+		get_tree().change_scene("res://Scene/win.tscn")
+	
+
+func _on_Countdown_timeout():
+	time_remaining -=1
+	if time_remaining <= 0:
+		get_tree().change_scene("res://Scene/GameOver.tscn")
+	else:
+		var m = time_remaining / 60
+		var s = time_remaining % 60
+		Remaining.text = str(m)+":"+str(s)
